14e6b4bd41a6fb00dd9e2a42c659cbc266f11cd2
Khoa <khoa.h0086881@gmail.com>
2026-09-16 12:41:50 +0700

    Fix: Temporarily commented out mock task card data to test deployment

--- src/data/mockTaskCardData.ts
+++ src/data/mockTaskCardData.ts
@@ -1,63 +1,63 @@
-import type { TaskDto } from "../types/TaskCardData";
-import { TaskPriority } from "../types/TaskPriority";
-import { TaskStatus } from "../types/TaskStatus";
+// import type { TaskDto } from "../types/TaskCardData";
+// import { TaskPriority } from "../types/TaskPriority";
+// import { TaskStatus } from "../types/TaskStatus";
 
-export const mockTaskCardData : TaskDto[] = [
-    {
-        taskId: "task-1",
-        title: "Clean the house",
-        description: "Full house cleaning before guests arrive.",
-        priority: TaskPriority.URGENT,
-        status: TaskStatus.INCOMPLETE,
-        dueDate: "2026-09-11",
-        dueTime: "18:00:00",
-        estimateMin: 45,
-        totalExp: 50,
-        subTasks: [
-            {
-                id: "subtask-1-1",
-                title: "Clean the toilet.",
-                expGain: 25,
-                position: 1,
-                isCompleted: true
-            },
-            {
-                id:"subtask-1-2",
-                title: "Sweep the house.",
-                expGain: 25,
-                position: 2,
-                isCompleted: false
-            }
+// export const mockTaskCardData : TaskDto[] = [
+//     {
+//         taskId: "task-1",
+//         title: "Clean the house",
+//         description: "Full house cleaning before guests arrive.",
+//         priority: TaskPriority.URGENT,
+//         status: TaskStatus.INCOMPLETE,
+//         dueDate: "2026-09-11",
+//         dueTime: "18:00:00",
+//         estimateMin: 45,
+//         totalExp: 50,
+//         subTasks: [
+//             {
+//                 id: "subtask-1-1",
+//                 title: "Clean the toilet.",
+//                 expGain: 25,
+//                 position: 1,
+//                 isCompleted: true
+//             },
+//             {
+//                 id:"subtask-1-2",
+//                 title: "Sweep the house.",
+//                 expGain: 25,
+//                 position: 2,
+//                 isCompleted: false
+//             }
 
-        ]
-    },
+//         ]
+//     },
 
-     {
-        taskId: "task-2",
-        title: "Clean the room",
-        description: "",
-        priority: TaskPriority.HIGH,
-        status: TaskStatus.INCOMPLETE,
-        dueDate: "2026-09-11",
-        dueTime: "18:00:00",
-        estimateMin: 20,
-        totalExp: 50,
-        subTasks: [
-            {
-                id: "subtask-2-1",
-                title: "Make the bed.",
-                expGain: 25,
-                position: 1,
-                isCompleted: true
-            },
-            {
-                id: "subtask-2-2",
-                title: "Sweep the house.",
-                expGain: 25,
-                position: 2,
-                isCompleted: false
-            }
+//      {
+//         taskId: "task-2",
+//         title: "Clean the room",
+//         description: "",
+//         priority: TaskPriority.HIGH,
+//         status: TaskStatus.INCOMPLETE,
+//         dueDate: "2026-09-11",
+//         dueTime: "18:00:00",
+//         estimateMin: 20,
+//         totalExp: 50,
+//         subTasks: [
+//             {
+//                 id: "subtask-2-1",
+//                 content: "Make the bed.",
+//                 exp: 25,
+//                 position: 1,
+//                 isCompleted: true
+//             },
+//             {
+//                 id: "subtask-2-2",
+//                 title: "Sweep the house.",
+//                 expGain: 25,
+//                 position: 2,
+//                 isCompleted: false
+//             }
 
-        ]
-    }
-]
+//         ]
+//     }
+// ]
